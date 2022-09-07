ca2187bbf14ada55e4a8cc5485386d5fccada676
Thomas Gauvin <35609369+thomasgauvin@users.noreply.github.com>
2022-09-07 17:25:26 -0400

    added out directory, removed .next

---
+++ out/_next/static/RGerwYN3wo80eAF4AkLqN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],"/catalog":["static/css/f5d3a53567d70f15.css","static/chunks/pages/catalog-a247a1c03580ec20.js"],sortedPages:["/_app","/_error","/catalog"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
