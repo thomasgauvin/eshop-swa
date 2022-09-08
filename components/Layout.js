311569c55d97e585f0c43f84a63842e2639ed678
Thomas Gauvin <35609369+thomasgauvin@users.noreply.github.com>
2022-09-08 11:29:33 -0400

    styling to show message showing...

--- components/Layout.js
+++ components/Layout.js
@@ -4,29 +4,32 @@ import styles from "../styles/Catalog.module.scss";
 export default function Layout({children}) {
   return (
     <div className={styles["footer-at-bottom"]}>
-        <header className={styles["esh-app-header"]}>
-            <div className={styles["container"]}>
-                <article className={styles["d-flex align-content-center justify-content-between"]}>
-                    <section>
-                        <a>
-                            <img className={styles["esh-app-header-brand"]} src="assets/images/logo_color.svg" />
-                        </a>
-                    </section>
+        <div>
+            <header className={styles["esh-app-header"]}>
+                <div className={styles["container"]}>
+                    <article className={styles["d-flex align-content-center justify-content-between"]}>
+                        <section>
+                            <a>
+                                <img className={styles["esh-app-header-brand"]} src="assets/images/logo_color.svg" />
+                            </a>
+                        </section>
 
-                    <section className={styles["d-flex align-content-center justify-content-between"]}>
-                        <esh-identity></esh-identity>
-                        <esh-basket-status></esh-basket-status>
-                    </section>
-                </article>
-            </div>
-            <div className={styles["esh-app-header-promo"]}>
-                <span className={styles["esh-app-header-promo-title"]}>All T-SHIRTS</span>
-                <span className={styles["esh-app-header-promo-subtitle"]}>On sale this weekend</span>
-            </div>
+                        <section className={styles["d-flex align-content-center justify-content-between"]}>
+                            <esh-identity></esh-identity>
+                            <esh-basket-status></esh-basket-status>
+                        </section>
+                    </article>
+                </div>
+                <div className={styles["esh-app-header-promo"]}>
+                    <span className={styles["esh-app-header-promo-title"]}>All T-SHIRTS</span>
+                    <span className={styles["esh-app-header-promo-subtitle"]}>On sale this weekend</span>
+                </div>
 
-        </header>
+            </header>
 
-        {children}
+            {children}
+
+        </div>
 
         <footer className={styles["esh-app-footer"]}>
             <div className={styles["container"]}>
