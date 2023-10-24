0d8fb766a424b32b1b6a0ecae54821ae1496b320
Thomas Gauvin <35609369+thomasgauvin@users.noreply.github.com>
2023-10-24 10:32:47 -0400

    added accentuation of discounts and sale

--- SWA/components/CatalogItem.js
+++ SWA/components/CatalogItem.js
@@ -11,9 +11,18 @@ export default function CatalogItem({ CatalogItem }) {
       <div style={{width: "15rem", height: "16rem", position: "relative"}}>
           <img src={`${CatalogItem.pictureUri}`} height={"100%"} width={"100%"} style={{objectFit:"cover"}}></img>
         </div>
-        <div style={{display: "flex", justifyContent: "space-between", margin: "1em", fontSize:"1.2em"}}>
+        <div style={{display: "flex", justifyContent: "space-between", marginTop: "0.2em", fontSize:"1.2em"}}>
             <div>{CatalogItem.name}</div>
-            <div style={{color:"#f66f61"}}>{formatter.format(CatalogItem.price)}</div>
+              { CatalogItem.previousPrice &&
+                <div style={{display:"flex"}}>
+                  <div style={{color:"#b9b9b9", textDecoration: "line-through"}}>{formatter.format(CatalogItem.price)}</div>
+                  <div style={{color:"#f66f61", paddingLeft: "0.4em", fontWeight: "600"}}>{formatter.format(CatalogItem.price)}</div>
+                </div>
+              }
+              {
+                !CatalogItem.previousPrice &&
+                <div style={{color:"#f66f61", paddingLeft: "0.4em", fontWeight: "600"}}>{formatter.format(CatalogItem.price)}</div>
+              }
         </div>
     </div>
   )
--- SWA/components/Layout.js
+++ SWA/components/Layout.js
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import styles from "../styles/Catalog.module.scss";
 
 export default function Layout({children}) {
